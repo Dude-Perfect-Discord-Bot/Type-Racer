5586428421aa369f7fb59b671a7aa728a6215a7b
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2021-05-16 18:17:33 +0530

    feat(font): Use the new font

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,14 @@
-const fs = require('fs');
 const { Canvas } = require('canvas-constructor');
-const data = fs.readFileSync('./src/utils/words.txt', 'utf-8').trim().split('\n').filter(Boolean);
+const data = [...Array(26)].map((_, y) => String.fromCharCode(y + 65));
 
-const generateText = (n) => data.sort(() => Math.random() - Math.random()).slice(0, n).join(' ');
+const generateText = (n) => data.sort(() => Math.random() - Math.random()).map((x, i) => x + data[i+1] + data[i+2]).slice(0, n).join(' ');
 
 const makeImage = async (baseImage, text) => {
     return new Canvas(1317, 655)
     .printImage(await baseImage, 0, 0, 1317, 655)
+    .setTextFont('120px RobotoRegular')
     .setColor('#000000')
-    .setTextSize(125)
-    .printText(text, 435, 465)
+    .printText(text, 407, 475)
     .save()
     .toBuffer();
 }
